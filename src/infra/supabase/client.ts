bfe3f5a9ead2457ed6d54431015aeef0db24c2c9
Rafael Santos <rafael.santos@grupomulti.com.br>
2025-04-24 19:24:36 -0300

    atualizando variaveis de ambiente

--- src/infra/supabase/client.ts
+++ src/infra/supabase/client.ts
@@ -3,10 +3,10 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = async (cookieStore: ReturnType<typeof cookies>) => {
-   const armazenamento = await cookieStore
+  const armazenamento = await cookieStore
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
